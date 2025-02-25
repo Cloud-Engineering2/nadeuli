281cf40eca15c78ea102fca0d221c0c5d8d0ea29
HIRedrain <redrain@yu.ac.kr>
2025-02-25 10:33:13 +0900

    :sparkles: feat : UserRole 생성

---
+++ src/main/java/nadeuli/entity/constant/UserRole.java
@@ -0,0 +1,33 @@
+package nadeuli.entity.constant;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.Arrays;
+
+@ToString
+@Getter
+public enum UserRole {
+    ADMIN("ROLE_ADMIN"),
+    MEMBER("ROLE_MEMBER");
+
+    private String userRole;
+
+    UserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public static UserRole getInstance(String userRole) { // userRole 의 값을 문자열로 찾는 함수
+
+        return Arrays.stream(UserRole.values()) // UserRole 값 -> 배열 -> 스트림
+                .filter(type -> type.getUserRole().equals(userRole)) // userRole 과 type(UserRole) 값 일치 여부로 정제
+                .findFirst() // 정제된 것에서 첫 번째 요소 반환
+                .orElseThrow(() -> {
+                    System.out.println("❌ getInstance() - 변환 실패.. 유효 x 값: " + userRole);
+                    return new IllegalArgumentException("getInstance() - Invalid UserRole : " + userRole);
+                }); // 값 x => 예외
+    }
+
+
+
+}
