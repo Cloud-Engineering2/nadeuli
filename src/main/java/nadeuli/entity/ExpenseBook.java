8b7743fe1c179024edcf57d059ae7782b468949e
minjko <komj36@gmail.com>
2025-02-25 14:57:38 +0900

    :pencil: fix: Expense book 관계 수정

--- src/main/java/nadeuli/entity/ExpenseBook.java
+++ src/main/java/nadeuli/entity/ExpenseBook.java
@@ -9,6 +9,7 @@
  * 작업자        날짜        수정 / 보완 내용
  * ========================================================
  * 이홍비    2025.02.25     생성자 + of() 추가
+ * 고민정    2025.02.25     생성자 접근수준, Itinerary 다대일->일대일 관계 수정
  *
  * ========================================================
  */
@@ -16,6 +17,7 @@
 package nadeuli.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,8 +28,8 @@ import org.hibernate.annotations.OnDeleteAction;
 
 @Getter
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Table(name = "expense_book")
 public class ExpenseBook extends BaseTimeEntity {
     @Id
@@ -35,7 +37,7 @@ public class ExpenseBook extends BaseTimeEntity {
     @Column(name = "ebid", nullable = false)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JoinColumn(name = "iid", nullable = false)
     private Itinerary iid;
@@ -49,18 +51,18 @@ public class ExpenseBook extends BaseTimeEntity {
     private Integer totalExpenses;
 
 
-    // 생성자
-    public ExpenseBook(Itinerary iid, Integer totalBudget, Integer totalExpenses) {
-
-        // 초기화
-        this.iid = iid;
-        this.totalBudget = totalBudget;
-        this.totalExpenses = totalExpenses;
-    }
+//    // 생성자
+//    public ExpenseBook(Itinerary iid, Integer totalBudget, Integer totalExpenses) {
+//
+//        // 초기화
+//        this.iid = iid;
+//        this.totalBudget = totalBudget;
+//        this.totalExpenses = totalExpenses;
+//    }
 
     // static factory method
     public static ExpenseBook of (Itinerary iid, Integer totalBudget, Integer totalExpenses) {
-        return new ExpenseBook(iid, totalBudget, totalExpenses);
+        return new ExpenseBook(null, iid, totalBudget, totalExpenses);
     }
 
 
