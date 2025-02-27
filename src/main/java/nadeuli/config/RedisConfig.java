3fc4744c5de3977cbeda6d2dd559d8500da1771a
kookrudals <kookrudals@gmail.com>
2025-02-27 17:36:43 +0900

    :sparkles: feat: redis 적용을 위한

---
+++ src/main/java/nadeuli/config/RedisConfig.java
@@ -0,0 +1,36 @@
+/* RedisConfig.java
+ * Redis 설정 클래스
+ * 해당 파일 설명
+ * 작성자 : 국경민
+ * 최초 작성 날짜 : 2025-02-27
+ *
+ * ========================================================
+ * 프로그램 수정 / 보완 이력
+ * ========================================================
+ * 작업자       날짜       수정 / 보완 내용
+ * ========================================================
+ * 국경민    2025-02-27   Redis 설정 및 RedisTemplate 빈 생성
+ * ========================================================
+ */
+
+package nadeuli.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+}
